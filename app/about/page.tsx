8e584375f0d9d7be721917e2b82bfaab798045b5
Rocky Li <rocky@terracotta.dev>
2025-06-12 16:05:59 +0800

    added about page

---
+++ app/about/page.tsx
@@ -0,0 +1,27 @@
+import { HeroHeader } from "@/components/header";
+// import HeroSection from "@/components/hero-section-one";
+import Image from "next/image";
+import ContentOne from "@/components/content-one";
+import FeaturesThree from "@/components/features-three";
+import FeaturesTwo from "@/components/features-two"
+import FeaturesFive from "@/components/features-five";
+import FooterSection from "@/components/footer-one";
+import CallToActionOne from "@/components/call-to-action-one";
+// import LogoCloudOne from "@/components/logo-cloud-one";
+import HeroSectionTwo from "@/components/hero-section-two";
+
+export default function Home() {
+  return (
+    <div>
+      {/* <HeroHeader /> */}
+      <HeroSectionTwo />
+      {/* <LogoCloudOne /> */}
+      <ContentOne />
+      <FeaturesThree />
+      <FeaturesFive />
+      <FeaturesTwo />
+      <CallToActionOne />
+      <FooterSection />
+    </div>
+  );
+}
