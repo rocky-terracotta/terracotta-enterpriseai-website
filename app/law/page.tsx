c9aad4c542b80e902bb11e73b65e15819d9eea2f
Rocky Li <rocky@terracotta.dev>
2025-06-25 16:33:25 +0800

    Added favicon.png

--- app/law/page.tsx
+++ app/law/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
       <FeaturesThree />
       <FeaturesEleven />
       <FeaturesFive />
-      <FeaturesTwo />
+      {/* <FeaturesTwo /> */}
       {/* <FAQsThree /> */}
       <CallToActionOne />
       <FooterSection />
